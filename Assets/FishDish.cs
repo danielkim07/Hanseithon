53a82819d0c470124be6733d5b1c518a061c6c3c
danielkim07 <spoon0727@kakao.com>
2025-07-24 01:05:34 +0900

    많은걸 수정함

---
+++ Assets/FishDish.cs
@@ -0,0 +1,55 @@
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FishDish : MonoBehaviour
+{
+    public Image leftHand;
+    public Sprite[] images;
+    private int currentIndex = 0;
+    private bool leave = false;
+
+    private void Update()
+    {
+        KeyDown();
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (leave)
+        {
+            Leave();
+        }
+    }
+
+    private void Leave()
+    {
+        transform.position = new Vector2 (transform.position.x - 100, transform.position.y);
+        if (transform.position.x <= -2000) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void KeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.J))
+        {
+            if (currentIndex < images.Length - 1)
+            {
+                currentIndex++;
+                leftHand.sprite = images[currentIndex - 1];
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space))
+        {
+            if (currentIndex == images.Length - 1)
+            {
+                leftHand.sprite = images[currentIndex];
+                leave = true;
+            }
+        }
+    }
+}
